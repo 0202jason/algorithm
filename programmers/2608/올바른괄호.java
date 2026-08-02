1d2548d8aa9f32477002a0432717d6359e5a845f
jaehyeon <zizon1818@gmail.com>
2026-08-02 14:57:16 +0900

    solve(programmers): 올바른 괄호

--- programmers/2608/올바른괄호.java
+++ programmers/2608/올바른괄호.java
@@ -5,6 +5,9 @@ class Solution {
         // 필요 없는 변수. 없애고 써도 될듯.
         boolean answer = true;
         
+        // Stack 선언. 메서드로는 push, pop, peak, size, isEmpty 등이 있다.
+        // 기본 자료구조는 vector
+        // 요즘은 Deque를 많이 쓴다네??
         Stack<Character> stack = new Stack<>();
         for (int i = 0; i < s.length(); i++){
             char c = s.charAt(i);
